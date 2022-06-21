5ffd764ccf98121baf8421827a688ac68fdaa095
Firman Styono <styonof@gmail.com>
2022-06-21 23:38:33 +0700

    Integration Socialite --Pending Menit ke-7

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        return view('auth.user.login');
+    }
+
+    public function google()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        return 'Test Provider OK';
+    }
+}
